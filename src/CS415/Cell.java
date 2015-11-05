4cdf0ad635f051900d0ad4ecdca332ca407f3686
Eric Spaeth <erspaeth@gmail.com>
2015-11-05 15:19:59 -0600

    Define Cell class

--- src/CS415/Cell.java
+++ src/CS415/Cell.java
@@ -1,5 +1,21 @@
 package CS415;
 
 public class Cell {
+	
+	private int state;
+	
+	public Cell(){
+		
+		state = 0;
+	}
+	
+	public int getState() {
+		
+		return state;
+	}
+	public void setState(int newState) {
+		
+		state = newState;
+	}
 
 }
