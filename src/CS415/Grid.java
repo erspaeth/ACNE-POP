a374390ef221eb661520b6c95bc4a0b74257e78e
GravelChi <achogarth@gmail.com>
2015-11-05 15:20:20 -0600

    added extends JFrame; ruleset method definition; and grid basics

--- src/CS415/Grid.java
+++ src/CS415/Grid.java
@@ -1,5 +1,16 @@
 package CS415;
 
 public class Grid {
-
+	
+	private boolean doesWrap;
+	private Cell[][] storedCells;
+	private int width, height;
+	
+	public Grid(int width, int height){}
+	
+	public Grid(Shape input){}
+	
+	public Cell getCell(int col, int row){return null;}
+	
+	
 }
