8b8534d515a37149b7a2b4ee48a943de812db83b
GravelChi <achogarth@gmail.com>
2015-11-06 17:30:55 -0600

    added singleton code to filemanager and shapecollection

--- src/CS415/FileManager.java
+++ src/CS415/FileManager.java
@@ -2,4 +2,17 @@ package CS415;
 
 public class FileManager {
 
+	private static FileManager instance = null;
+
+	protected FileManager() {
+		// Exists only to defeat instantiation.
+	}
+
+	public static FileManager getInstance() {
+		if (instance == null) {
+			instance = new FileManager();
+		}
+		return instance;
+	}
+	
 }
