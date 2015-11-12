ee811ed1a81b5dc7933ed6bdcb55d9943131647e
GravelChi <achogarth@gmail.com>
2015-11-12 16:18:11 -0600

    still working on GUI, able to draw grid, some simulation constructors,

--- src/CS415/SimControl.java
+++ src/CS415/SimControl.java
@@ -1,6 +1,28 @@
 package CS415;
 import javax.swing.*;
+import java.awt.*;
 
 public class SimControl extends JFrame{
 
+	Display display;
+	
+	public SimControl(Simulation sim){
+		
+		//get current grid from simulation
+		Grid current = sim.getCurrentGrid();
+		
+		//create display from grid
+		display = new Display(current);
+		
+		//get iterations
+		
+		setTitle("Grid Display");
+        setSize(700, 700);
+        setLayout(new BorderLayout());
+        add(display, BorderLayout.CENTER);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+	}
+	
 }
--- src/CS415/Simulation.java
+++ src/CS415/Simulation.java
@@ -2,4 +2,21 @@ package CS415;
 
 public class Simulation {
 
+	private Grid startGrid, currentGrid;
+	int iterations;
+	
+	public Simulation(Grid start, Grid current, int iter){
+		startGrid = start;
+		currentGrid = current;
+		iterations = iter;
+	}
+	
+	public Simulation(Grid fresh){
+		startGrid = currentGrid = fresh;
+		iterations = 0;
+	}
+	
+	public Grid getCurrentGrid(){
+		return currentGrid;
+	}
 }
