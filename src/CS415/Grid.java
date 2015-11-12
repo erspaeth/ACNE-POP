01114479ec428ea35197d6f36d91796b951f3e5f
Eric Spaeth <erspaeth@gmail.com>
2015-11-12 14:28:48 -0600

    Defining basics of Grid class.

--- src/CS415/Grid.java
+++ src/CS415/Grid.java
@@ -6,11 +6,79 @@ public class Grid {
 	private Cell[][] storedCells;
 	private int width, height;
 	
-	public Grid(int width, int height){}
+	public Grid(int width, int height) {
+		
+		this.width = width;
+		this.height = height;
+		
+		storedCells = new Cell[width][height];
+		
+		for(int i = 0; i < width; i++) {
+			
+			for(int j = 0; j < height; j++) {
+				
+				storedCells[i][j] = new Cell();
+			}
+		}
+	}
 	
+	//TODO Define this constructor
 	public Grid(Shape input){}
 	
-	public Cell getCell(int col, int row){return null;}
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
 	
+	public Cell getCell(int row, int col) {
+		
+		
+		return storedCells[row][col];
+	}
+	
+	public String toString() {
+		
+		String sReturn;
+		
+		StringBuffer sbBuffer = new StringBuffer(200);
+		
+		for(int i = 0; i < height; i++) {
+			
+			sbBuffer.append("\n");
+			for(int j = 0; j < width; j++) {
+				
+				sbBuffer.append( storedCells[i][j].getState() ); 				
+			}
+		}
+		
+		sbBuffer.append("\n");
+		sReturn = sbBuffer.toString();
+		return sReturn;
+	}
+	
+	
+	
+	
+    public static void main(String[] args) {
+    	
+    	Grid g = new Grid(10, 10);
+    	
+    	Cell c;
+    	
+    	for(int i = 0; i < 10; i++) {
+    		
+    		c = g.getCell(i, 2);
+    		c.setState(2);
+    		c = g.getCell(i, 6);
+    		c.setState(2);
+    
+    	}
+    	System.out.println("hello");
+        System.out.println(g.toString()); // Display the string.
+    }
 	
 }
+
+
